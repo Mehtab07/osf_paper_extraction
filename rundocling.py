ee77fbf5e1b3672df16222d9f79ce86bf00b123d
Mehtab07 <smehtabshah1996@gmail.com>
2025-05-06 11:48:37 +0200

    docling modified

--- rundocling.py
+++ rundocling.py
@@ -1,41 +1,11 @@
 from docling.document_converter import DocumentConverter
 
-source = "test/2sz48.pdf"  # document per local path or URL
+source = "test/7h94n.pdf"  # document per local path or URL
 converter = DocumentConverter()
 result = converter.convert(source)
 markdown_content = result.document.export_to_markdown()
 
-with open("docling.md","w", encoding="utf-8") as file:
+with open("results/docling.md","w", encoding="utf-8") as file:
     file.write(markdown_content)
 
 print("Markdown content saved to docling.md")  
-
-# Markdown file to save
-# output_path = "paper_output.md"
-
-# with open(output_path, "w", encoding="utf-8") as f:
-#     # Title
-#     if result.title:
-#         f.write(f"# {result.title}\n\n")
-
-#     # Authors
-#     if result.authors:
-#         authors_str = ", ".join(result.authors)
-#         f.write(f"**Authors:** {authors_str}\n\n")
-
-#     # DOI
-#     if result.doi:
-#         f.write(f"**DOI:** {result.doi}\n\n")
-
-#     # Abstract
-#     if result.abstract:
-#         f.write("## Abstract\n\n")
-#         f.write(result.abstract + "\n\n")
-
-#     # Sections
-#     if result.sections:
-#         for section in result.sections:
-#             if section.title:
-#                 f.write(f"## {section.title}\n\n")
-#             if section.text:
-#                 f.write(section.text.strip() + "\n\n")
